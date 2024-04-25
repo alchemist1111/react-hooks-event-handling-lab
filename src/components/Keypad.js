28ca0def32b45d12c2d500440b3e1af557047256
alchemist1111 <emoyoeugine@gmail.com>
2024-04-25 11:39:13 +0300

    adds content components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange() {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" name="search" placeholder="" onChange={handleChange}/>
+        </div>
     )
 }
 
